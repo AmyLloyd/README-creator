b49bd18e4e4fb27ac1ff49e78fc0b48198e35a26
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-08 10:48:43 +1030

    install and uninstall inquirer with new version checking suitability to current environment

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const inquirer = require("inquirer");
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your name?", "What is your gitHub username?",
+    "What is your name?", 
+    "What is your gitHub username?",
     "Which languages are you proficient in?",
     "What are your strengths as a developer?",
     "What are your strengths as a team member?",
     "What do you want to achieve in your work?"
 ];
 
+inquirer.prompt([
+])
+.then((answers) => {
+
+})
+
+.catch((errror) => {
+    if(error.isTtyError) {
+        //Prompt couldn't be rendered int he current environment
+    } else {
+        //Something else went wrong
+    } 
+});
+
+for (const question of questions) {
+    console.log(question);
+}
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const data = data.JSON.stringify(data, null, "\t"), (err) =>
+    data = data.JSON.stringify(data, null, "\t"), (err) =>
     err ? console.log(err) : console.log("Success!")
     fileName = "README.md";
 
-    fs.writeFile();
+};
+//refer to documentation https://www.npmjs.com/package/inquirer#inquirercreatepromptmodule---prompt-function
+// TODO: Create a function to initialize app
+function init() {
+   
 };
 
-// TODO: Create a function to initialize app
-function init(questions) {
-    inquirer
-    .createPromptModule([
-        for (i = 0; i < questions.length; i++) {
-            type: "input",
-            message: `${questions[i]}`
-        }
-    ])
-    .then((data)) => {
-        writeToFile(fileName, data);      
 
-    }
-}
+
 
 // Function call to initialize app
 init();
